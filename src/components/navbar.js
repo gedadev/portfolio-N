a090016e4e9892348cec1a2db8e392aaac17e7ec
Gedaias Jair <gedaias@geda.dev>
2023-10-12 20:32:47 -0600

    Add media queries
    Create a function that generates a hamburger menu which will be visible in 700px screens
    Add the listeners to toggle an active class in this menu
    Also add a listener to remove the active class when the link is clicked
    Add the styles to arrange the menu when the screen is 700px or less
    Add another media query for 1000px screens to reduce the spacing in the navbar elements

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,34 @@
-export default function createNavbar() {
+function createHamburgerMenu(navMenu, navLinks) {
+  const hamburgerMenu = document.createElement('div');
+  hamburgerMenu.className = 'ham-menu';
+
+  for (let index = 0; index < 3; index += 1) {
+    const bar = document.createElement('span');
+    bar.className = 'bar';
+    hamburgerMenu.appendChild(bar);
+  }
+
+  hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+
+  navLinks.forEach((item) => {
+    item.addEventListener('click', () => {
+      hamburgerMenu.classList.remove('active');
+      navMenu.classList.remove('active');
+    });
+  });
+
+  return hamburgerMenu;
+}
+
+function createNavbar() {
   const navbar = document.createElement('nav');
   const branding = document.createElement('h1');
   const menu = document.createElement('ul');
   const navItems = ['Home', 'About', 'Projects', 'Contact'];
+  const navLinks = [];
 
   navbar.className = 'navbar';
   branding.className = 'branding';
@@ -18,10 +44,14 @@ export default function createNavbar() {
     navLink.innerText = item;
     navItem.appendChild(navLink);
     menu.appendChild(navItem);
+    navLinks.push(navLink);
   });
 
   navbar.appendChild(branding);
   navbar.appendChild(menu);
+  navbar.appendChild(createHamburgerMenu(menu, navLinks));
 
   return navbar;
 }
+
+export default createNavbar;
