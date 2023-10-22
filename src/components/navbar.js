b677b5b5d78bfeaa1f879c469483058d202be071
Gedaias Jair <gedaias@geda.dev>
2023-10-22 15:07:23 -0600

    Reorganize the code in a class in order to add new functions

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,57 +1,57 @@
-function createHamburgerMenu(navMenu, navLinks) {
-  const hamburgerMenu = document.createElement('div');
-  hamburgerMenu.className = 'ham-menu';
+export default class Navbar {
+  static createNavbar() {
+    const navbar = document.createElement('nav');
+    const branding = document.createElement('h1');
+    const menu = document.createElement('ul');
+    const navItems = ['Home', 'About', 'Projects', 'Contact'];
+    const navLinks = [];
 
-  for (let index = 0; index < 3; index += 1) {
-    const bar = document.createElement('span');
-    bar.className = 'bar';
-    hamburgerMenu.appendChild(bar);
+    navbar.className = 'navbar';
+    branding.className = 'branding';
+    branding.innerText = 'Geda';
+    menu.className = 'nav-menu';
+
+    navItems.forEach((item) => {
+      const navItem = document.createElement('li');
+      const navLink = document.createElement('a');
+      navItem.className = 'nav-item';
+      navLink.className = 'nav-link';
+      navLink.href = `#${item}`;
+      navLink.innerText = item;
+      navItem.appendChild(navLink);
+      menu.appendChild(navItem);
+      navLinks.push(navLink);
+    });
+
+    navbar.appendChild(branding);
+    navbar.appendChild(menu);
+    navbar.appendChild(Navbar.createHamburgerMenu(menu, navLinks));
+
+    return navbar;
   }
 
-  hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('active');
-    navMenu.classList.toggle('active');
-  });
+  static createHamburgerMenu(navMenu, navLinks) {
+    const hamburgerMenu = document.createElement('div');
+    hamburgerMenu.className = 'ham-menu';
 
-  navLinks.forEach((item) => {
-    item.addEventListener('click', () => {
-      hamburgerMenu.classList.remove('active');
-      navMenu.classList.remove('active');
+    for (let index = 0; index < 3; index += 1) {
+      const bar = document.createElement('span');
+      bar.className = 'bar';
+      hamburgerMenu.appendChild(bar);
+    }
+
+    hamburgerMenu.addEventListener('click', () => {
+      hamburgerMenu.classList.toggle('active');
+      navMenu.classList.toggle('active');
     });
-  });
 
-  return hamburgerMenu;
+    navLinks.forEach((item) => {
+      item.addEventListener('click', () => {
+        hamburgerMenu.classList.remove('active');
+        navMenu.classList.remove('active');
+      });
+    });
+
+    return hamburgerMenu;
+  }
 }
-
-function createNavbar() {
-  const navbar = document.createElement('nav');
-  const branding = document.createElement('h1');
-  const menu = document.createElement('ul');
-  const navItems = ['Home', 'About', 'Projects', 'Contact'];
-  const navLinks = [];
-
-  navbar.className = 'navbar';
-  branding.className = 'branding';
-  branding.innerText = 'Geda';
-  menu.className = 'nav-menu';
-
-  navItems.forEach((item) => {
-    const navItem = document.createElement('li');
-    const navLink = document.createElement('a');
-    navItem.className = 'nav-item';
-    navLink.className = 'nav-link';
-    navLink.href = `#${item}`;
-    navLink.innerText = item;
-    navItem.appendChild(navLink);
-    menu.appendChild(navItem);
-    navLinks.push(navLink);
-  });
-
-  navbar.appendChild(branding);
-  navbar.appendChild(menu);
-  navbar.appendChild(createHamburgerMenu(menu, navLinks));
-
-  return navbar;
-}
-
-export default createNavbar;
