a7e895a030f761c93d75e77291a887ac62ce8ac9
Gedaias Jair <gedaias@geda.dev>
2023-10-25 22:25:01 -0600

    Convert the footer into a sidebar to retain the channels in the viewport

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,63 +1,99 @@
-export default function createFooter() {
-  const footerContainer = document.createElement('div');
-  const creditsContainer = document.createElement('div');
-  const credits = document.createElement('p');
-  const channelsContainer = document.createElement('div');
-  const channelsTitle = document.createElement('p');
-  const channelList = document.createElement('ul');
-  const channels = {
-    github: {
-      name: 'github',
-      link: 'https://github.com/gedadev',
-      icon: '<i class="fa-brands fa-github"></i>',
-    },
-    linkedin: {
-      name: 'linkedin',
-      link: 'https://www.linkedin.com/in/gedabg/',
-      icon: '<i class="fa-brands fa-linkedin"></i>',
-    },
-    twitter: {
-      name: 'twitter',
-      link: 'https://twitter.com/gedauwu',
-      icon: '<i class="fa-brands fa-square-x-twitter"></i>',
-    },
-    email: {
-      name: 'email',
-      link: 'gedaias@geda.dev',
-      icon: '<i class="fa-solid fa-envelope"></i>',
-    },
-    resume: {
-      name: 'resume',
-      link: '',
-      icon: '<i class="fa-solid fa-file-lines"></i>',
-    },
-  };
+export default class Footer {
+  static channels;
 
-  footerContainer.className = 'footer-container';
-  creditsContainer.className = 'credits-container';
-  channelsContainer.className = 'channels-container';
-  channelsTitle.className = 'channels-title';
-  channelList.className = 'channels';
+  static channelList;
 
-  channelsTitle.innerText = 'Find me online';
-  credits.innerHTML = '2023 <i class="fa-regular fa-copyright"></i> | Designed and built by Gedaias Jair';
+  static createFooter() {
+    const footerContainer = document.createElement('div');
 
-  Object.values(channels).forEach((item) => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.href = item.link;
-    a.title = item.name;
-    li.innerHTML = item.icon;
-    a.appendChild(li);
-    channelList.appendChild(a);
-  });
+    footerContainer.className = 'footer-container';
 
-  channelsContainer.appendChild(channelsTitle);
-  channelsContainer.appendChild(channelList);
-  creditsContainer.appendChild(credits);
+    footerContainer.appendChild(Footer.createChannels());
+    footerContainer.appendChild(Footer.createCredits());
 
-  footerContainer.appendChild(channelsContainer);
-  footerContainer.appendChild(creditsContainer);
+    Footer.moveChannels();
 
-  return footerContainer;
+    return footerContainer;
+  }
+
+  static createChannels() {
+    const channelsContainer = document.createElement('div');
+    const channelsTitle = document.createElement('p');
+    const channelList = document.createElement('ul');
+    const channels = {
+      github: {
+        name: 'github',
+        link: 'https://github.com/gedadev',
+        icon: '<i class="fa-brands fa-github"></i>',
+      },
+      linkedin: {
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/in/gedabg/',
+        icon: '<i class="fa-brands fa-linkedin"></i>',
+      },
+      twitter: {
+        name: 'twitter',
+        link: 'https://twitter.com/gedauwu',
+        icon: '<i class="fa-brands fa-square-x-twitter"></i>',
+      },
+      email: {
+        name: 'email',
+        link: 'gedaias@geda.dev',
+        icon: '<i class="fa-solid fa-envelope"></i>',
+      },
+      resume: {
+        name: 'resume',
+        link: '',
+        icon: '<i class="fa-solid fa-file-lines"></i>',
+      },
+    };
+
+    channelsContainer.className = 'channels-container';
+    channelsTitle.className = 'channels-title';
+    channelList.className = 'channels';
+    channelsTitle.innerText = 'Find me online';
+
+    Object.values(channels).forEach((item) => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = item.link;
+      a.title = item.name;
+      li.innerHTML = item.icon;
+      a.appendChild(li);
+      channelList.appendChild(a);
+    });
+
+    channelsContainer.appendChild(channelsTitle);
+    channelsContainer.appendChild(channelList);
+
+    Footer.channels = channelsContainer;
+    Footer.channelList = channelList;
+
+    return channelsContainer;
+  }
+
+  static createCredits() {
+    const creditsContainer = document.createElement('div');
+    const credits = document.createElement('p');
+
+    creditsContainer.className = 'credits-container';
+    credits.innerHTML = '2023 <i class="fa-regular fa-copyright"></i> | Designed and built by Gedaias Jair';
+
+    creditsContainer.appendChild(credits);
+
+    return creditsContainer;
+  }
+
+  static moveChannels() {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY
+        > (document.body.scrollHeight - document.documentElement.clientHeight) * 0.98) {
+        Footer.channels.style.position = 'static';
+        Footer.channelList.style.flexDirection = 'row';
+      } else {
+        Footer.channels.style.position = 'fixed';
+        Footer.channelList.style.flexDirection = 'column';
+      }
+    });
+  }
 }
