baade1b26701f6ec56924d87609ebdc9842a4c67
Gedaias Jair <gedaias@geda.dev>
2023-10-27 20:23:55 -0600

    Reorganize code

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,64 +1,71 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable prefer-destructuring */
-export default function createContact() {
-  const contactSection = document.createElement('section');
-  const sectionTitle = document.createElement('h2');
-  const formContainer = document.createElement('div');
-  const formEngageText = document.createElement('p');
-  const contactForm = document.createElement('form');
-  const submitFormButton = document.createElement('button');
-  const formInputs = {
-    name: {
-      label: document.createElement('label'),
-      input: document.createElement('input'),
-    },
-    email: {
-      label: document.createElement('label'),
-      input: document.createElement('input'),
-    },
-    message: {
-      label: document.createElement('label'),
-      input: document.createElement('textarea'),
-    },
-  };
+export default class Contact {
+  static createContactSection() {
+    const contactSection = document.createElement('section');
+    const sectionTitle = document.createElement('h2');
 
-  contactSection.id = 'Contact';
-  sectionTitle.className = 'contact-section-title';
-  formContainer.className = 'form-container';
-  formEngageText.className = 'engage-msg';
-  contactForm.id = 'contact-form';
-  submitFormButton.className = 'submit-btn';
+    contactSection.id = 'contact';
+    sectionTitle.className = 'contact-section-title';
+    sectionTitle.innerText = 'Ready to collaborate?';
 
-  formInputs.name.input.type = 'text';
-  formInputs.name.input.placeholder = 'John Doe';
-  formInputs.email.input.type = 'email';
-  formInputs.email.input.placeholder = 'jdoe@domain.com';
-  formInputs.message.input.placeholder = 'Start typing...';
-  submitFormButton.type = 'submit';
-  submitFormButton.innerHTML = 'Send <i class="fa-solid fa-share-from-square"></i>';
-  submitFormButton.setAttribute('form', 'contact-form');
+    contactSection.appendChild(sectionTitle);
+    contactSection.appendChild(Contact.createForm());
 
-  sectionTitle.innerText = 'Ready to collaborate?';
-  formEngageText.innerText = 'Leave me a message, or you can reach me by my online channels in the sidebar';
+    return contactSection;
+  }
 
-  Object.entries(formInputs).forEach((formInput) => {
-    const inputContainer = document.createElement('div');
-    inputContainer.className = 'input-container';
-    formInput[1].label.innerText = `Your ${formInput[0]}: (*)`;
-    formInput[1].label.htmlFor = formInput[0];
-    formInput[1].input.id = formInput[0];
-    formInput[1].input.setAttribute('autocomplete', 'off');
-    formInput[1].input.required = true;
-    inputContainer.appendChild(formInput[1].label);
-    inputContainer.appendChild(formInput[1].input);
-    contactForm.appendChild(inputContainer);
-  });
-  formContainer.appendChild(formEngageText);
-  formContainer.appendChild(contactForm);
-  formContainer.appendChild(submitFormButton);
+  static createForm() {
+    const formContainer = document.createElement('div');
+    const formEngageText = document.createElement('p');
+    const contactForm = document.createElement('form');
+    const submitFormButton = document.createElement('button');
+    const formInputs = {
+      name: {
+        label: document.createElement('label'),
+        input: document.createElement('input'),
+      },
+      email: {
+        label: document.createElement('label'),
+        input: document.createElement('input'),
+      },
+      message: {
+        label: document.createElement('label'),
+        input: document.createElement('textarea'),
+      },
+    };
 
-  contactSection.appendChild(sectionTitle);
-  contactSection.appendChild(formContainer);
+    formContainer.className = 'form-container';
+    formEngageText.className = 'engage-msg';
+    contactForm.id = 'contact-form';
+    submitFormButton.className = 'submit-btn';
+    formInputs.name.input.type = 'text';
+    formInputs.name.input.placeholder = 'John Doe';
+    formInputs.email.input.type = 'email';
+    formInputs.email.input.placeholder = 'jdoe@domain.com';
+    formInputs.message.input.placeholder = 'Start typing...';
+    submitFormButton.type = 'submit';
+    submitFormButton.innerHTML = 'Send <i class="fa-solid fa-share-from-square"></i>';
+    submitFormButton.setAttribute('form', 'contact-form');
+    formEngageText.innerText = 'Leave me a message, or you can reach me by my online channels';
 
-  return contactSection;
+    Object.entries(formInputs).forEach((formInput) => {
+      const inputContainer = document.createElement('div');
+      inputContainer.className = 'input-container';
+      formInput[1].label.innerText = `Your ${formInput[0]}: (*)`;
+      formInput[1].label.htmlFor = formInput[0];
+      formInput[1].input.id = formInput[0];
+      formInput[1].input.setAttribute('autocomplete', 'off');
+      formInput[1].input.required = true;
+      inputContainer.appendChild(formInput[1].label);
+      inputContainer.appendChild(formInput[1].input);
+      contactForm.appendChild(inputContainer);
+    });
+
+    formContainer.appendChild(formEngageText);
+    formContainer.appendChild(contactForm);
+    formContainer.appendChild(submitFormButton);
+
+    return formContainer;
+  }
 }
