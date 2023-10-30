62e093f2f62634fde98fb42daa002fbebca6a89e
Gedaias Jair <gedaias@geda.dev>
2023-10-29 22:22:49 -0600

    Add the sections and components to the document

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import './styles/style.css';
 import './styles/header.css';
 import './styles/home.css';
-import createNavbar from './components/navbar.js';
-import createHome from './components/home.js';
+import './styles/about.css';
+import './styles/projects.css';
+import './styles/contact.css';
+import './styles/footer.css';
+import Navbar from './components/navbar.js';
+import Home from './components/home.js';
+import About from './components/about.js';
+import Projects from './components/projects.js';
+import Contact from './components/contact.js';
+import Footer from './components/footer.js';
 
 const header = document.querySelector('header');
 const main = document.querySelector('main');
+const footer = document.querySelector('footer');
 
-header.appendChild(createNavbar());
-main.appendChild(createHome());
+header.appendChild(Navbar.createNavbar());
+main.appendChild(Home.createHomeSection());
+main.appendChild(About.createAboutSection());
+main.appendChild(Projects.createProjectsSection());
+main.appendChild(Contact.createContactSection());
+footer.appendChild(Footer.createFooter());
+
+const navbar = new Navbar(main.childNodes);
+navbar.createScrollspy();
